40368b9772f538f7de4c844c1f194d441a35f1fb
jonschlinkert <jon.schlinkert@sellside.com>
2017-08-30 20:20:03 -0400

    add example

---
+++ examples/inquirer.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var dim = require('ansi-dim');
+var List = require('..');
+
+/**
+ * Example prompt from Inquirer
+ */
+
+var prompt = new List({
+  type: 'list',
+  name: 'theme',
+  message: 'What do you want to do?',
+  choices: [
+    'Order a pizza',
+    'Make a reservation',
+    new List.Separator(dim(' ─────────────')),
+    'Ask for opening hours',
+    {
+      name: 'Contact support',
+      disabled: 'Unavailable at this time'
+    },
+    'Talk to the receptionist'
+  ]
+},
+{
+  type: 'list',
+  name: 'size',
+  message: 'What size do you need?',
+  choices: ['Jumbo', 'Large', 'Standard', 'Medium', 'Small', 'Micro'],
+  filter: function (val) {
+    return val.toLowerCase();
+  }
+});
+
+prompt.run()
+  .then(function(answer) {
+    console.log(JSON.stringify(answer, null, '  '));
+  });
